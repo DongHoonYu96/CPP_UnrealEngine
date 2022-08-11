7ed1b83b931b98ba85abea7696a03525030348aa
dbehdgns71@naver.com <dbehdgns71@naver.com>
2022-08-11 22:20:37 +0900

    0811

--- TestUnrealEngine/Source/TestUnrealEngine/MyGameModeBase.cpp
+++ TestUnrealEngine/Source/TestUnrealEngine/MyGameModeBase.cpp
@@ -2,11 +2,20 @@
 
 
 #include "MyGameModeBase.h"
-#include "MyPawn.h"
+//#include "MyPawn.h"
 #include "MyCharacter.h"
 
 //기본생성자
 AMyGameModeBase::AMyGameModeBase()
 {
-	DefaultPawnClass = AMyCharacter::StaticClass();
+	//DefaultPawnClass = AMyCharacter::StaticClass();
+
+	//FClassFinder생성자(찾을클래스)
+	static ConstructorHelpers::FClassFinder<ACharacter>
+		BP_Char(TEXT("Blueprint'/Game/BluePrints/BP_MyCharacter.BP_MyCharacter_C'"));
+
+	if (BP_Char.Succeeded())
+		DefaultPawnClass = BP_Char.Class;
+
+
 }
